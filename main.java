296e7e287419b6ae3a8e62ef350553d312f44884
CosteaAndreiIonut <100835117+CosteaAndreiIonut@users.noreply.github.com>
2022-03-09 22:27:06 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,17 @@
+package forme;
+
+import forme.fereastra;
+import forme.linie;
+import forme.triunghi;
+import forme.cerc;
+import forme.patrulater;
+import forme.poligon;
+
+public class main {
+
+	public static void main(String[] args) {
+		fereastra f1 = new fereastra();
+		
+	}
+
+}
